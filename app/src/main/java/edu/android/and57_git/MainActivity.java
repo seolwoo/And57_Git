4e128e49b74fefb781dae4b93cb69014e09d90c6
seolwoo <seolfeel@naver.com>
2017-09-14 10:49:48 +0900

    seolwoo project

--- app/src/main/java/edu/android/and57_git/MainActivity.java
+++ app/src/main/java/edu/android/and57_git/MainActivity.java
@@ -5,9 +5,17 @@ package edu.android.and57_git;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void test() {
+        // TODO : ??
+    }
+
 }
